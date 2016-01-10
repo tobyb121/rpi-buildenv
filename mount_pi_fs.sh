133a8bf06740a6ab33d517ae2c85cd3ae1421d89
tobyb121 <tobyb121@hotmail.co.uk>
2016-01-10 18:10:16 +0000

    Added input validation and usage info for fs mounting script

--- mount_pi_fs.sh
+++ mount_pi_fs.sh
@@ -1,6 +1,28 @@
 #! /bin/bash
 set -e
 
+usage() {
+   echo "Mount img file to local filesystem" 1>&2
+   echo "Usage" 1>&2
+   echo "  $0 [img file]" 1>&2
+   echo "This script must be run as root" 1>&2
+   exit 1
+}
+
+if [ $# -ne 1 ]; then
+   usage
+fi
+
+if [ -f "%1" ]; then
+   usage
+fi
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+
 FDISK=`fdisk -l $1`
 
 SECTOR_SIZE=`echo "$FDISK" | sed -n 's/Units: sectors of [0-9]* \* [0-9]* = \([0-9]*\) bytes.*/\1/p'`
